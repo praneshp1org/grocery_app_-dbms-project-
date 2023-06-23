664a164d1f2a7fc196b0a04513b53b09e55cbb06
Pranesh Pyara Shrestha <info.praneshpyara>
2023-06-23 23:57:56 +0545

    database design done

---
+++ backend/products_dao.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='password@',
+                              host='127.0.0.1',
+                              database='grocery_store')
+cursor = cnx.cursor()
+query = "SELECT * FROM grocery_store.products"
+cursor.execute(query)
+for (product_id, name, uom_id, price_per_unit) in cursor:
+    print(product_id, name, price_per_unit)
+cnx.close()
