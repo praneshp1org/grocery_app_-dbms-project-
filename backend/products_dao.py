3bd5b6cf35ab4f6a63b57ad5227bda400176f24c
Pranesh Pyara Shrestha <info.praneshpyara>
2023-06-24 15:19:11 +0545

    retrieved data from the database

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -4,8 +4,8 @@ cnx = mysql.connector.connect(user='root', password='password@',
                               host='127.0.0.1',
                               database='grocery_store')
 cursor = cnx.cursor()
-query = "SELECT * FROM grocery_store.products"
+query = "SELECT products.products_id, products.price_per_unit, products.name, products.uom_id, uom.uom_name FROM grocery_store.products inner join grocery_store.uom on products.uom_id=uom.uom_id"
 cursor.execute(query)
-for (product_id, name, uom_id, price_per_unit) in cursor:
-    print(product_id, name, price_per_unit)
+for (product_id, name, uom_id, price_per_unit, uom_name) in cursor:
+    print(product_id,name, price_per_unit, uom_name)
 cnx.close()
