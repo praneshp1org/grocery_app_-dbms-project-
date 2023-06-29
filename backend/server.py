f87814052e763f2c53e988130fc0e90af23bc63e
Pranesh Pyara Shrestha <info.praneshpyara>
2023-06-29 13:46:28 +0545

    Products page done, edit functionality to be added!

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,34 @@
 from flask import Flask, request, jsonify
 import products_dao
+import uom_dao
+import json
 from sql_connection import get_sql_connection
 
 app = Flask(__name__)
 
 connection = get_sql_connection()
 
+@app.route('/getUOM', methods=['GET'])
+def get_uom():
+    response = uom_dao.get_uoms(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 @app.route('/getProducts', methods=['GET'])
 def get_Products():
     products = products_dao.get_all_products(connection)
     response = jsonify(products)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/insertProduct', methods=['POST'])
+def insert_product():
+    request_payload = json.loads(request.form['data'])
+    product_id = products_dao.insert_new_product(connection, request_payload)
+    response = jsonify({
+        'product_id': product_id
+    })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
